726cbef8b70218ea099eed87a836e8172d2689d1
IshanKapoor12 <94053414+IshanKapoor12@users.noreply.github.com>
2023-06-16 02:38:14 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/bmiCalculator.html", function(req, res){
+    res.sendFile(__dirname + "/bmiCalculator.html");
+});
+
+app.post("/bmiCalculator.html", function(req, res){
+    var n1 = parseFloat(req.body.w);
+    var n2 = parseFloat(req.body.h);
+    var n = n1/(n2*n2);
+    res.send("Your BMI is: " + n);
+});
+
+app.listen(3000, function(){
+    console.log("The server has started on port 3000.");
+});

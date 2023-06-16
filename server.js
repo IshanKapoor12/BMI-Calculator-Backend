9236f87e51083fd7628448db85c1a7cbc91ee2f7
IshanKapoor12 <94053414+IshanKapoor12@users.noreply.github.com>
2023-06-16 14:45:52 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const bodyParser = require("body-parser");
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get("/bmiCalculator.html", function(req, res){
+app.get("/", function(req, res){
     res.sendFile(__dirname + "/bmiCalculator.html");
 });
 
-app.post("/", function(req, res){
+app.post("/bmiCalculator.html", function(req, res){
     var n1 = parseFloat(req.body.w);
     var n2 = parseFloat(req.body.h);
     var n = n1/(n2*n2);
